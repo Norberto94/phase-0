ebc6c045e39067dc3f7a259a6144b97ed3853779
Norberto <you@example.com>
2016-01-19 01:15:31 -0500

    add my work except for variable-methods.rb file as I have to add the links to my other work first inside the file

---
+++ week-4/math/my_solution.rb
@@ -0,0 +1,30 @@
+# Math Methods
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def add(num_1, num_2)
+ num_1 + num_2
+end
+
+def subtract(num_1, num_2)
+ num_1 - num_2
+end
+
+def multiply(num_1, num_2)
+ num_1 * num_2
+end
+
+def divide(num_1, num_2)
+ num_1.to_f / num_2
+end
+
+puts add 5, 2
+
+puts subtract 8, 3
+
+puts multiply 12, 7
+
+puts divide 9, 2
