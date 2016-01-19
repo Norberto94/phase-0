82891cb43df059595a919eb976504177da9bce51
Norberto <you@example.com>
2016-01-19 16:51:57 -0500

    add my solution for challenge 4.5.2

---
+++ week-4/calculate-grade/my_solution.rb
@@ -0,0 +1,19 @@
+# Calculate a Grade
+
+# I worked on this challenge [ with: Alyssa Page ].
+
+
+# Your Solution Below
+def get_grade (num)
+	if num >= 90
+		return "A"
+	elsif 90 > num  && num >= 80
+		return "B"
+	elsif 80 > num && num >= 70
+		return "C"
+	elsif 70 > num && num >= 60
+		return "D"
+	else 
+		return "F"
+	end
+end
