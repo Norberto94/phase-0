6650e940ca5e6a6d921df04653e386cf997a150c
Norberto <you@example.com>
2016-01-19 16:01:13 -0500

    add my solution to concatenation problem

---
+++ week-4/concatenate-arrays/my_solution.rb
@@ -0,0 +1,12 @@
+# Concatenate Two Arrays
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def array_concat(array_1, array_2)
+  puts array_1.join(' , ') + array_2.join(' , ') 
+end
+
+array_concat(["Hello", "World"], ["!"])
