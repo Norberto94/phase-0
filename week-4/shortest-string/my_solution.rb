c0d845a743cec479674d81de36bc7fb3da4e4c07
Norberto <you@example.com>
2016-01-19 23:24:47 -0500

    add my corrected solution

--- week-4/shortest-string/my_solution.rb
+++ week-4/shortest-string/my_solution.rb
@@ -12,31 +12,10 @@
 
 #Your Solution Below
 def shortest_string(list_of_words)
-	if list_of_words == []
-		puts nil
-	elsif list_of_words == [" "]
-		puts " "
-	elsif list_of_words[0].length < list_of_words[1].length && list_of_words[0].length < list_of_words[2].length
-		puts list_of_words[0] + " is the shortest string"
-	elsif list_of_words[1.length] < list_of_words[2].length && list_of_words[1].length < list_of_words[0].length
-		puts list_of_words[1] + " is the shortest string"
-	else list_of_words[2].length < list_of_words[1].length && list_of_words[2].length < list_of_words[0].length
-		puts list_of_words[2] + " is the shortest string"
+	if list_of_words == [""]
+		return ""
+	else list_of_words.sort_by! {|x| x.length}
+		return list_of_words[0]
 	end
 end
-
-shortest_string(["abc", "dell", "helloworld"])
-
-=begin
-list_of_words.each do |word|
-	word.length
-	if word[0].length < word[1].length && word[0].length < word[2].length
-		puts word[0] + " is the shortest word"
-	elsif word[1].length < word[0].length && word[1].length < word[2].length
-		puts word[1] + " is the shortest word"
-	else word[2].length < word[1].length && word[2].length < word[1].length
-		puts word[2] + " is the shortest word"
-	end
-end
-=end
 
