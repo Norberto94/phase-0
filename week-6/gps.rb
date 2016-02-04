de132920bc15e394fe860093b8d93815d43951f0
Norberto <you@example.com>
2016-02-04 00:06:58 -0500

    add my finsihed 2.3 gps

--- week-6/gps.rb
+++ week-6/gps.rb
@@ -1,34 +1,33 @@
 # Your Names
-# 1)
-# 2)
+# 1)John Craigie
+# 2)Norberto Caceres
 
-# We spent [#] hours on this challenge.
+# We spent [1] hour on this challenge.
+
+#for each key in library divide value by remaining ingredients
+
+#iterate through the hash to check if the input is not a key
 
 # Bakery Serving Size portion calculator.
 
 def serving_size_calc(item_to_make, num_of_ingredients)
   library = {"cookie" => 1, "cake" =>  5, "pie" => 7}
-  error_counter = 3
 
-  library.each do |food|
-    if library[food] != library[item_to_make]
-      error_counter += -1
+  unless library.has_key? (item_to_make)
+      raise ArgumentError.new("#{item_to_make} is not a valid input")
     end
-  end
 
-  if error_counter > 0
-    raise ArgumentError.new("#{item_to_make} is not a valid input")
-  end
-
-  serving_size = library.values_at(item_to_make)[0]
-  remaining_ingredients = num_of_ingredients % serving_size
-
+  serving_size = library[item_to_make]
+  remaining_ingredients = num_of_ingredients % serving_size 
+      
+  
   case remaining_ingredients
   when 0
     return "Calculations complete: Make #{num_of_ingredients / serving_size} of #{item_to_make}"
+  when 1..4
+    return "Calculations complete: Make #{num_of_ingredients / serving_size} of #{item_to_make}, you have #{remaining_ingredients} leftover ingredients. Why not make cookies?"
   else
-    return "Calculations complete: Make #{num_of_ingredients / serving_size} of #{item_to_make}, you have #{remaining_ingredients} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
-  end
+    return "Calculations complete: Make #{num_of_ingredients / serving_size} of #{item_to_make}, you have #{remaining_ingredients} leftover ingredients. Why not make a cake?"
 end
 
 p serving_size_calc("pie", 7)
@@ -40,3 +39,22 @@ p serving_size_calc("cookie", 10)
 p serving_size_calc("THIS IS AN ERROR", 5)
 
 #  Reflection
+=begin
+What did you learn about making code readable by working on this challenge?
+
+In order to iterate the part of the code that will raise an error if improper input is given, I learned about the method .has_key? which made it so we did not need to have a counter which was uncessesarily complex
+
+Did you learn any new methods? What did you learn about them?
+
+I learned about .has_key? which is a conditional that checks if the input is a key in the hash
+
+What did you learn about accessing data in hashes? 
+
+Well, the .has_key? iterates through the keys of a hash, something that pops up quite often.
+
+
+What concepts were solidified when working through this challenge?
+
+I learned quite a bit about case, I had already seen it in Codeacademy but it had been a while so I feel like I will be using it more often from now on as it seems very neat
+
+=end
